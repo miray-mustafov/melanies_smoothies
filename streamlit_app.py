1ec9c67ce042998faef7399430d163837d5d89fc
Miray Mustafov <mustafovmiray@gmail.com>
2025-05-07 10:48:28 +0300

    container width2

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ if time_to_insert:
     st.success(f'{name_on_order}, your Smoothie is ordered!', icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json(), use_container_width=True)
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
